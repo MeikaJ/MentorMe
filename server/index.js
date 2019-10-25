730c9a29c7f5059f3f8674b5f22b0ec6d62e66db
meikaj <meikajprince@gmail.com>
2019-10-25 15:42:49 -0700

    updated src files

--- server/index.js
+++ server/index.js
@@ -1,11 +1,19 @@
 import express from 'express';
+import dbConfig from './config/db'
+
+
+
 const app = express();
+// database placeholder
+
+dbConfig();
+
 const PORT = process.env.PORT || 8080;
 
 app.listen(PORT, err => {
     if (err) {
         console.log("error")
     } {
-        console.log(`App listen to port:${PORT} `);
+        console.log(`App listen to port:${PORT}`);
     }
 }) 
