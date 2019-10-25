da094d6fa11c6f0f5df398e9209317cbf0337e88
meikaj <meikajprince@gmail.com>
2019-10-25 15:52:21 -0700

    dependency update

--- server/config/db.js
+++ server/config/db.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 export default () => {
     mongoose.Promise = global.Promise;
     mongoose.connect('mongod://localhost/mentorMe');
-    mongoose.connection()
+    mongoose.connection
         .once('open', () => console.log('running mongodb'))
         .on('error', err => console.error(err));
 };
