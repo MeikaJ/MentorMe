e48ba89379daac45727152f1f9ca540e35d524c2
meikaj <meikajprince@gmail.com>
2019-10-28 19:33:27 -0700

    added git ignore file

--- server/index.js
+++ server/index.js
@@ -5,17 +5,14 @@ import middleware from './config/middleware';
 import { MeetingRoutes } from './modules'
 
 
-
 const app = express();
 // database placeholder
 
 dbConfig();
-
 // middleware
 
 middlewareConfig(app);
 app.use('/api', [MeetingRoutes]);
-
 
 const PORT = process.env.PORT || 8080;
 
