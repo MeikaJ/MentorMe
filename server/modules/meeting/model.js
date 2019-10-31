74efc36fae087f44fcfa9d48102c7e739b1d7d8c
meikaj <meikajprince@gmail.com>
2019-10-30 18:24:34 -0700

    file folder change

--- server/modules/meeting/model.js
+++ server/modules/meeting/model.js
@@ -1,13 +1,17 @@
 import mongoose, { schema } from 'mongoose';
 var meetSchema = new mongoose.Schema({
-    title: {
-        type: String,
-        require: true
-    },
-    description: {
+  title: {
+    type: String,
+    require: true
+  },
+  description: {
 
-        type: String,
-        require: true
-    }
-})
+    type: String,
+    require: true
+  },
+  eventDate: {
+    type: date
+
+  }
+}, { timestamp: true })
 export default mongoose.model('meetup', meetSchema);
